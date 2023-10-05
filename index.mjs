b94d594851b96dd9318d3550e77c41f54179f9e3
pavelpakhnutow <pavelpakhnutow@gmail.com>
2023-10-05 09:59:51 +0300

    cloun

--- index.mjs
+++ index.mjs
@@ -69,6 +69,13 @@ const start = async () => {
 
         if (text === '/tag@Pokyr_Casino_Bot' || text === '/tag'){
 
+            console.log(msg)
+
+            if (chatId === -1001672307901) {
+                console.log('ааааа')
+
+                await bot.sendMessage(chatId, '🤡',{reply_to_message_id: msg.message_id})
+            }
             return bot.sendMessage(from,'Че делать будем нахуй? Кого тэгать?', tagOptions)
 
         }
